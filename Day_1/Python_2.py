81184175271329c622ec0b2d6f9b7007bfce07ef
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-12 23:39:30 -0400

    input

--- Day_1/Python_2.py
+++ Day_1/Python_2.py
@@ -3,12 +3,12 @@
 # Script name Assignment 100 Days of  Python
 # Author Name Christen Reinhart
 # Date of Latest Revision 06/12/2024
-# Sources https://chatgpt.com/share/a6a7d219-97e7-4cd7-9511-274cf26dcf1b
+# Sources Udemy
 # Purpose In Python, Print message  
 
 #Begin
 
 #print first message
-print("Hello World!")
+print("Hello!" + input("What is yourName?"))
 
 #End
