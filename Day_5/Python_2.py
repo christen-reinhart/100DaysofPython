fc527fb51ec24df02edbdc9d4b04a8e9fa225c21
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 10:00:54 -0400

    highest_score

--- Day_5/Python_2.py
+++ Day_5/Python_2.py
@@ -5,3 +5,16 @@
 # Date of Latest Revision 06/13/2024
 # Sources Auditorium
 # Purpose In Python, Loops
+
+# Input a list student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+    
+highest_score = 0 
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+        # print(highest_score)
+        
+print(f"The highest score in the class is: {highest_score}")
