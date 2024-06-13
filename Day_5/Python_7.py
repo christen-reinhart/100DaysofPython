572736d26a2333ba4b9486b98a6a10cd4508c117
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 12:32:53 -0400

    Python6

---
+++ Day_5/Python_7.py
@@ -0,0 +1,31 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/13/2024
+# Sources Auditorium/ Udemy
+# Purpose In Python, Password Generator
+
+# For Loop with Password Range target = 100
+
+print("Welcome to the Password Gengerator")
+nr_letters = int(input("How many letter would you like in password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many number would you like?\n"))
+
+# Eazy Level
+password = ""
+# nr_letter = 4
+for char in range(1, nr_letters + 1)
+    # 1 - 4
+    random_char = random.choice(letters)
+    password += random_char
+    print(password)
+
+
+
+
+
+
+
+
