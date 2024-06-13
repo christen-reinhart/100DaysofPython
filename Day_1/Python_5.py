906b3e74710589e82d5f6f4044200e1e2cc861f5
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 00:47:19 -0400

    len

---
+++ Day_1/Python_5.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/12/2024
+# Sources Udemy
+# Purpose In Python, Project 
+
+#1 Create an greeting 
+print("welcome to my program")
+#2 Ask for the name of a city where you grew up
+city = input("Which city did you grow up in ?")
+#3 aSK NAME OF A Pet
+pet = input("What was the name of a pet?")
+#4 Combine the city and pet name
+print("Your band could be " + city + " " + pet)
