2b1892e57752b3357fa614ffe06e70b3dc623ff2
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 08:52:53 -0400

    Interger

--- Day_2/Python_5.py
+++ Day_2/Python_5.py
@@ -4,4 +4,11 @@
 # Author Name Christen Reinhart
 # Date of Latest Revision 06/13/2024
 # Sources Auditorium
-# Purpose In Python, BMI Calculation
+# Purpose In Python, Calculate years left
+
+age = input()
+
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
