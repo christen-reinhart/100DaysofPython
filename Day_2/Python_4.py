95be0f1a9cded1f2cc755e9b1376a6f843b1c4db
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 08:34:47 -0400

    Interger

--- Day_2/Python_4.py
+++ Day_2/Python_4.py
@@ -4,4 +4,18 @@
 # Author Name Christen Reinhart
 # Date of Latest Revision 06/13/2024
 # Sources Auditorium
-# Purpose In Python,
+# Purpose In Python, BMI Calculation
+
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# Don't change the code above
+
+# Write your code below this line
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+bmi = weight_as_int / (height_as_float * height_as_float)
+bmi_as_int = int(bmi)
+print(bmi_as_int)
