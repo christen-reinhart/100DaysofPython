63a7a5169da52a62440accd4185117f0cdeacf45
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 00:03:26 -0400

    input

---
+++ Day_1/Python_3.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/12/2024
+# Sources Udemy
+# Purpose In Python, Print message  
+
+#Begin
+
+num1 = int(input(4))
+num2 = int(input(5))
+
+print(num1 * num2)
