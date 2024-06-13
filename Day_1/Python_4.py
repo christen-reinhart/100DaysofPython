d1e770e6d3327a92492f2f457f4ea78500e075bf
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 00:13:45 -0400

    len

--- Day_1/Python_4.py
+++ Day_1/Python_4.py
@@ -1,2 +1,12 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/12/2024
+# Sources Udemy
+# Purpose In Python, Use len to count letters  
+
+#Begin
+
 numOfLetters = len("Angela")
 print(numOfLetters)
