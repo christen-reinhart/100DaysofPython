36f10754b215a81fd97dd41ac856adbd041d71da
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 09:06:12 -0400

    Interger

---
+++ Day_2/Python_6.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/13/2024
+# Sources Auditorium
+# Purpose In Python, Calculate Bill
+
+# Welcome to bill calculator
+print("Welcome to the trips calculator")
+
+#Each person should pay
+bill = float(input("What was the total bill?$"))
+
+# Calculate Tip
+tip = int(input("How much would you like to give? 10, 12, or 15?"))
+
+# How many people 
+people = int(input("How many people split the bill?"))
+
+bill_with_tip = tip / 100 * bill + bill
+print(bill_with_tip)
