d7e14e9e8cb46a9dce82d3ec1220750b1a75f6cd
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 12:43:25 -0400

    Python6

--- Day_5/Python_7.py
+++ Day_5/Python_7.py
@@ -6,7 +6,12 @@
 # Sources Auditorium/ Udemy
 # Purpose In Python, Password Generator
 
-# For Loop with Password Range target = 100
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')']
+
 
 print("Welcome to the Password Gengerator")
 nr_letters = int(input("How many letter would you like in password?\n"))
