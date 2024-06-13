3156a6d0154a52faafa9735e6f534163fe81316d
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-12 23:32:08 -0400

    2

---
+++ Day_1/Python_2.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/12/2024
+# Sources https://chatgpt.com/share/a6a7d219-97e7-4cd7-9511-274cf26dcf1b
+# Purpose In Python, Print message  
+
+#Begin
+
+#print first message
+print("Hello World!")
+
+#End
