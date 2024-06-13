99615ad0dbae3f85735d60c61105269de1d74790
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 08:04:15 -0400

    Interger

--- Day_2/Python_3.py
+++ Day_2/Python_3.py
@@ -2,6 +2,10 @@
 
 # Script name Assignment 100 Days of  Python
 # Author Name Christen Reinhart
-# Date of Latest Revision 06/12/2024
+# Date of Latest Revision 06/13/2024
 # Sources Auditorium
-# Purpose In Python, Print message  
+# Purpose In Python, 
+
+num_char = len(input("what is your name?"))
+               
+                 
