ff88fa89c63cf64ce7561181526f26c6fe9e2a13
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 09:14:54 -0400

    Interger

--- Day_2/Python_6.py
+++ Day_2/Python_6.py
@@ -17,7 +17,11 @@ tip = int(input("How much would you like to give? 10, 12, or 15?"))
 
 # How many people 
 people = int(input("How many people split the bill?"))
-
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
 bill_with_tip = tip / 100 * bill + bill
 
-print(bill_with_tip)
+print(f"Each person should pay: ${final_amount}")
