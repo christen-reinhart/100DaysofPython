2173c3703612a63e1af83c82a48bf50e33898040
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 10:29:38 -0400

    Loop3

--- Day_5/Python_3.py
+++ Day_5/Python_3.py
@@ -4,4 +4,18 @@
 # Author Name Christen Reinhart
 # Date of Latest Revision 06/13/2024
 # Sources Auditorium
-# Purpose In Python, Loops
+# Purpose In Python, Calculate student height using Loops
+
+total_height = 0 
+for height in student_heights:
+    total_height += height
+print(f"total_height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students =+ 1
+print(f"number of students = {number_of_students}") 
+
+average_height = round(total_height / number_of_students) 
+print(f"average height = {average_height}")  
+    
