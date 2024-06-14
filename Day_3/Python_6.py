ed3b11c3d47e71eba3ee042ccdad4952484921fc
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-14 10:26:48 -0400

    Python12345

--- Day_3/Python_6.py
+++ Day_3/Python_6.py
@@ -1,33 +1,40 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 
-# Script name Assignment 100 Days of  Python
-# Author Name Christen Reinhart
-# Date of Latest Revision 06/14/2024
-# Sources Auditorium/ Udemy
-# Purpose In Python, if, else, condition
+# Script name: Assignment 100 Days of Python
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 06/14/2024
+# Sources: Auditorium/ Udemy
+# Purpose: In Python, if, else, condition
 
 # Multiple If Statements
 
 print("Welcome to Python Pizza")
-size = int(input('What is your size pizza?'))
-add_pepperoni = int(input('Do you want pepperoni?'))
-add_cheese = int(input('Do you want cheese?'))
+size = input('What size pizza do you want? (S, M, L): ').upper()
+add_pepperoni = input('Do you want pepperoni? (Y/N): ').upper()
+add_cheese = input('Do you want extra cheese? (Y/N): ').upper()
 
 bill = 0
+
+# Calculate base price based on pizza size
 if size == "S":
-  bill += 15
+    bill += 15
 elif size == "M":
-  bill += 20
+    bill += 20
+elif size == "L":
+    bill += 25
 else:
-  bill += 25
+    print("Invalid size selected.")
 
+# Add cost for pepperoni if selected
 if add_pepperoni == "Y":
-  if size == "S":
-    bill += 2
-  else:
-    bill += 3
+    if size == "S":
+        bill += 2
+    elif size in ["M", "L"]:
+        bill += 3
 
-if extra_cheese == "Y":
-  bill += 1
+# Add cost for extra cheese if selected
+if add_cheese == "Y":
+    bill += 1
 
+# Print the final bill
 print(f"Your final bill is: ${bill}.")
