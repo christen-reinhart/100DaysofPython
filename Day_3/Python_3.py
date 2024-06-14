6d298f1573c59e8b0bbe2d4a51fa17feda34fdbe
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-14 08:27:33 -0400

    Python12345

---
+++ Day_3/Python_3.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/14/2024
+# Sources Auditorium/ Udemy
+# Purpose In Python, if, else, condition
+
+# check height conditional
+
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in inches?"))
+
+if height > 48:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you will have to wait till you are taller")
