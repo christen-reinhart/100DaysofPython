2b9138f5804b49309d16b184c68a769d4d52b2b9
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-14 08:17:33 -0400

    day_3

---
+++ Day_3/Python_1.py
@@ -0,0 +1,33 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/13/2024
+# Sources Auditorium/ Udemy
+# Purpose In Python, Password Generator
+
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')']
+
+
+print("Welcome to the Password Gengerator")
+nr_letters = int(input("How many letter would you like in password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many number would you like?\n"))
+
+# Eazy Level
+password = ""
+# nr_letter 
+for char in range(1, nr_letters + 1):
+    password += random.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    random_char = random.choice(numbers)
+    password += random_char
+    print(password)
