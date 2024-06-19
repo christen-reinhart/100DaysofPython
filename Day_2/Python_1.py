7f76d917cf3c3e9748580dda5b9610cecdfaee60
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-19 16:37:59 -0400

    !

---
+++ Day_2/Python_1.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of  Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/12/2024
+# Sources Auditorium
+# Purpose In Python, Print message  
+
+#Begin
+
+
+
+
+
+print("Hello" + "World!")
+
+#End
