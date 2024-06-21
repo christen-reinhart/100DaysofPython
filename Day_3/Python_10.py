1497e5c35f91c8c23817b62725a13178ca069db0
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-21 11:12:27 -0400

    Python10

---
+++ Day_3/Python_10.py
@@ -0,0 +1,9 @@
+#!/user/bin/env python3
+
+# Script name Assignment 100 Days of Python
+# Author Name Christen Reinhart
+# Date of Latest Revision 06/14/2024
+# Sources Auditorium/ Udemy
+# Purpose In Python, if, else, and, or, not
+
+# Logical Operators
