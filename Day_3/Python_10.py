0aa0b3065b3c2a8d15503b0746f762f1c2cdef96
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-21 11:42:10 -0400

    Python10

--- Day_3/Python_10.py
+++ Day_3/Python_10.py
@@ -21,10 +21,10 @@ u = lower_names.count("u")
 e = lower_names.count("e")
 first_digit = t + r + u + e 
 
-t = lower_names.count("l")
-t = lower_names.count("o")
-t = lower_names.count("v")
-t = lower_names.count("e")
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
 second_digit = l + o + v + e
 
 score = int(str(first_digit) + str(second_digit))
