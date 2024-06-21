9290bf37b7b7d50d6037abde4e529ca0a886d58c
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-21 11:16:52 -0400

    Python10

--- Day_3/Python_10.py
+++ Day_3/Python_10.py
@@ -2,8 +2,11 @@
 
 # Script name Assignment 100 Days of Python
 # Author Name Christen Reinhart
-# Date of Latest Revision 06/14/2024
+# Date of Latest Revision 06/21/2024
 # Sources Auditorium/ Udemy
 # Purpose In Python, if, else, and, or, not
 
 # Logical Operators
+
+name1 = (input("What is your name?"))
+name1 = (input("What is their name?"))
